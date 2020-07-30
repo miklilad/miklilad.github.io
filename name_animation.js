a3c27f3812d107d66da05faba4f796a73cbeb05d
miklilad <ladislav@miklik.eu>
2020-07-30 18:39:19 +0200

    Name animation

---
+++ name_animation.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function animateSvg (id, animationLength, delay, delayIncrement){
+        const logo = document.getElementById(id);
+        const logoPaths = document.querySelectorAll(`#${id} path`);
+        for(let i = 0; i < logoPaths.length;i++){
+            logoPaths[i].style.strokeDasharray  = logoPaths[i].getTotalLength();
+            logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+            logoPaths[i].style.animation = `line-anim ${animationLength}s ease forwards ${delay}s`;
+            delay+=delayIncrement;
+        }
+        delay += animationLength;
+        logo.querySelector("g").style.animation = `fill 0.5s ease forwards ${delay}s`;
+    }
+// Set the id of SVG, delay time in seconds to start animation and delay between each animation
+    animateSvg('name', 2, 0, 0.2)
+}, false);
